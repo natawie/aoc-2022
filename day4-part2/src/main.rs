5e919597f987a61c3ee0f8ec9cf59c98cb2e186d
Natalia Łotocka <flotocki002@gmail.com>
2022-12-10 17:58:06 +0100

    add day 4

---
+++ day4-part2/src/main.rs
@@ -0,0 +1,24 @@
+use std::fs;
+use std::ops::RangeInclusive;
+
+static PATH: &'static str = "input.txt";
+
+fn main() {
+    let mut count: u32 = 0;
+    let contents = fs::read_to_string(PATH).expect("Something went wrong reading the file");
+    let mut values: Vec<&str> = contents.split("\n").collect();
+    values.pop();
+    let values: Vec<Vec<&str>> = values.iter().map(|x| x.split(",").collect()).collect();
+    let values: Vec<Vec<RangeInclusive<u32>>>= values.iter().map(|x| {
+        x.iter().map(|y| {
+        let y = y.split("-").collect::<Vec<&str>>();
+        y[0].parse::<u32>().unwrap()..=y[1].parse::<u32>().unwrap()
+        }).collect()
+    }).collect();
+    for group in values {
+        if group[0].contains(group[1].start()) || group[0].contains(group[1].end()) || group[1].contains(group[0].start()) || group[1].contains(group[0].end()) {
+            count += 1;
+        }
+    }
+    println!("{}", count);
+}
