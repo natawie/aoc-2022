3e2b7ff71c36aeb58ea2cfb8f9f9c0db6a3cf2d7
Natalia Łotocka <flotocki002@gmail.com>
2022-12-10 17:25:23 +0100

    fix and add part 2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -4,8 +4,10 @@ fn main() {
     let path = "input.txt";
     let contents = fs::read_to_string(path).expect("Something went wrong reading the file");
     let groups: Vec<Vec<&str>> = contents.split("\n\n").map(|x| x.split("\n").collect()).collect();
-    let mut groups: Vec<Vec<u32>> = groups.iter().map(|x| x.iter().map(|y| y.parse().unwrap_or(0)).collect()).collect();
+    let groups: Vec<Vec<u32>> = groups.iter().map(|x| x.iter().map(|y| y.parse().unwrap_or(0)).collect()).collect();
     let mut groups: Vec<u32> = groups.iter().map(|x| x.iter().sum()).collect();
     groups.sort();
-    println!("{:?}", groups);
+    groups.reverse();
+    println!("part 1: {:?}", groups[0]);
+    println!("part 2: {:?}", groups[0] + groups[1] + groups[2]);
 }
